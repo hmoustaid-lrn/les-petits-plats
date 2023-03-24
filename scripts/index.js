0fb18914d47c4da301fe5aee03e678bd37c1254f
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-03-24 15:55:24 +0000

    Add search functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,52 @@
+let recipes = []
+let filteredRecipes = []
+const searchbar = document.getElementById("searchbar")
+
+
 async function getRecipes() {
     const response = await fetch ('data/recipes.json');
-    const recipes = await response.json();
-    return recipes;
+    recipes = await response.json();
 }
 
-async function displayRecipes(recipes) {
+function searchRecipes() {
+	const search = searchbar.value.toLowerCase()
+	if (!search || !search.length) {
+		filteredRecipes = [...recipes]
+	} else {
+		filteredRecipes = [];
+		for (let recipe of recipes) {
+			if (recipe.name.toLowerCase().includes(search) || recipe.ingredients.find((ing) => ing.ingredient.toLowerCase().includes(search)) || recipe.description.toLowerCase().includes(search)) {
+				filteredRecipes.push(recipe)
+			}
+		}
+	
+	}
+    displayRecipes();
+}
+
+function registerSearchbarEvents(){
+    searchbar.addEventListener('input', searchRecipes)
+    const searchicon = document.getElementById("searchicon")
+	searchicon.onclick = searchRecipes
+}
+
+function displayRecipes() {
     const recipeSection = document.getElementById('recipes');
     recipeSection.innerHTML = '';
     recipeSection.style.gridTemplateRows = `repeat(${Math.ceil(recipes.length / 3)}, 365px)`
 	const timerIcon = document.createElement('i')
 	timerIcon.classList.add('fa-regular')
 	timerIcon.classList.add('fa-clock')
-    for (const recipe of recipes) {
+    for (const recipe of filteredRecipes) {
       const recipeCard = getRecipeCard(recipe);
       recipeSection.appendChild(recipeCard);
     }
 }
 
 async function init() {
-    const { recipes } = await getRecipes();
-    displayRecipes(recipes);
+    await getRecipes();
+    searchRecipes();
+    registerSearchbarEvents()
 }
   
 init();
