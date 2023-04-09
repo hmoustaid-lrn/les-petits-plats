0d3a198a23673655cb51febe7b20e52c124a8bb8
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-04-09 06:44:46 +0000

    Enhance search filter by adding inner for loop for ingredient iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,83 +1,93 @@
-let recipes = []
-let filteredRecipes = []
+let recipes = [];
+let filteredRecipes = [];
 
-const searchbar = document.getElementById("searchbar")
-const MIN_CHARS_TO_LAUNCH_SEARCH = 3
+const searchbar = document.getElementById("searchbar");
+const MIN_CHARS_TO_LAUNCH_SEARCH = 3;
 
-const recipeSection = document.getElementById('recipes');
+const recipeSection = document.getElementById("recipes");
 
 async function getRecipes() {
-    const response = await fetch ('data/recipes.json');
-    recipes = await response.json();
+  const response = await fetch("data/recipes.json");
+  recipes = await response.json();
 }
 
-function firstLoadingOfRecipes(){
-    filteredRecipes = [...recipes]
-    displayRecipes()
+function firstLoadingOfRecipes() {
+  filteredRecipes = [...recipes];
+  displayRecipes();
 }
 
 function searchRecipesInSearchbar() {
-    closeAllMenus()
-	const search = searchbar.value.toLowerCase()
-	if (search.length < MIN_CHARS_TO_LAUNCH_SEARCH) {
-		filteredRecipes = [...recipes]
-	} else {
-		filteredRecipes = [];
-		for (let i = 0; i < recipes.length; i++) {
-			if (
-                recipes[i].name.toLowerCase().includes(search) ||
-                recipes[i].ingredients.find(ing => ing.ingredient.toLowerCase().includes(search)) ||
-                recipes[i].description.toLowerCase().includes(search)
-              ) {
-                filteredRecipes.push(recipes[i]);
-              }
-              
-		}
-	
-	}
-    //Il est nécessaire de permettre la combinaison des recherches avec les tags et les recherches à l'aide de la barre de recherche
-    searchRecipesWithTags()
+  closeAllMenus();
+  const search = searchbar.value.toLowerCase();
+  if (search.length < MIN_CHARS_TO_LAUNCH_SEARCH) {
+    filteredRecipes = [...recipes];
+  } else {
+    filteredRecipes = [];
+    for (let i = 0; i < recipes.length; i++) {
+      let isMatch = false;
+      if (recipes[i].name.toLowerCase().indexOf(search) !== -1) {
+        isMatch = true;
+      } else if (recipes[i].description.toLowerCase().indexOf(search) !== -1) {
+        isMatch = true;
+      } else {
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+          if (recipes[i].ingredients[j].ingredient.toLowerCase().indexOf(search) !== -1) {
+            isMatch = true;
+            break;
+          }
+        }
+      }
+      if (isMatch) {
+        filteredRecipes.push(recipes[i]);
+      }
+    }
+  }
+  //Il est nécessaire de permettre la combinaison des recherches avec les tags et les recherches à l'aide de la barre de recherche
+  searchRecipesWithTags();
 }
 
-function registerSearchbarEvents(){
-    searchbar.addEventListener('input', searchRecipesInSearchbar)
-    const searchicon = document.getElementById("searchicon")
-	searchicon.onclick = searchRecipesInSearchbar
+function registerSearchbarEvents() {
+  searchbar.addEventListener("input", searchRecipesInSearchbar);
+  const searchicon = document.getElementById("searchicon");
+  searchicon.onclick = searchRecipesInSearchbar;
 }
 
 function displayRecipes() {
-    recipeSection.innerHTML = ''
-    if (!filteredRecipes.length) {
-        console.log(filteredRecipes);
-        handleErrorDiv(true);
-        return;
-    }
-    handleErrorDiv(false);
-    recipeSection.style.gridTemplateRows = `repeat(${Math.ceil(recipes.length / 3)}, 365px)`
-	const timerIcon = document.createElement('i')
-	timerIcon.classList.add('fa-regular')
-	timerIcon.classList.add('fa-clock')
-    for (const recipe of filteredRecipes) {
-      const recipeCard = getRecipeCard(recipe);
-      recipeSection.appendChild(recipeCard);
-    }
+  recipeSection.innerHTML = "";
+  if (!filteredRecipes.length) {
+    console.log(filteredRecipes);
+    handleErrorDiv(true);
+    return;
+  }
+  handleErrorDiv(false);
+  recipeSection.style.gridTemplateRows = `repeat(${Math.ceil(
+    recipes.length / 3
+  )}, 365px)`;
+  const timerIcon = document.createElement("i");
+  timerIcon.classList.add("fa-regular");
+  timerIcon.classList.add("fa-clock");
+  for (const recipe of filteredRecipes) {
+    const recipeCard = getRecipeCard(recipe);
+    recipeSection.appendChild(recipeCard);
+  }
 }
 
 function handleErrorDiv(display) {
-    if (display) {
-        errorDiv.textContent = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.';
-        recipeSection.style.gridTemplateRows = 'unset';
-        errorDiv.style.display = 'inherit';
-    } else {
-        errorDiv.style.display = 'none';
-    }
+  if (display) {
+    errorDiv.textContent =
+      "Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.";
+    recipeSection.style.gridTemplateRows = "unset";
+    errorDiv.style.display = "inherit";
+  } else {
+    errorDiv.style.display = "none";
+  }
 }
 
 async function init() {
-    await getRecipes()
-    firstLoadingOfRecipes()
-    registerSearchbarEvents()
-    registerMenusEvents()
+  await getRecipes();
+  firstLoadingOfRecipes();
+  registerSearchbarEvents();
+  registerMenusEvents();
 }
-  
+
 init();
